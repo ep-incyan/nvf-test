5cb093a5d1e7f3a08db62773ab5d1269adee0e62
Edward Potter <ep@incyan.com>
2025-12-04 17:04:02 +0000

    Step 4

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 {pkgs ? import <nixpkgs> {}}: let
-  nvfFlake = builtins.getFlake "github:ep-incyan/nvf-test";
+  nvfFlake = builtins.getFlake "github:ep-incyan/nvf-test/295e6750e7afd0e13d6be8f9c9e63c52d1158438";
   customNeovim = nvfFlake.packages."aarch64-darwin".default;
 in
   pkgs.mkShell {
